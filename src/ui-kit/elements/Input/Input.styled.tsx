7db7d3880f6baa00b6bf1de0e4c517979f0fca78
Ekaterina Zakimatova <ezakimatova@gmail.com>
2020-11-03 18:41:55 +0300

    improve input styles

--- src/ui-kit/elements/Input/Input.styled.tsx
+++ src/ui-kit/elements/Input/Input.styled.tsx
@@ -5,16 +5,16 @@ const Field = styled.input`
 	width: 100%;
 	height: 5vh;
 	padding: 1vh;
-	
+
 	color: ${ (p: StyledProps) => p.theme.colors.white };;
 
 	border: solid 2px ${ (p: StyledProps) => p.theme.colors.transparentWhite };
-	border-radius: 0.25vh;
+	border-radius: 0.7vh;
 
 	background: ${ (p: StyledProps) => p.theme.colors.invisibleWhite };
-	
+
 	transition: border-color 0.4s;
-	
+
 	&:focus {
 		border-color: ${ (p: StyledProps) => p.theme.colors.lightMain };
 	}
