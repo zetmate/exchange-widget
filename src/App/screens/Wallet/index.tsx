5bfcc2df846915eec6ca7540a60b3bf92c6bab1e
Ekaterina Zakimatova <ezakimatova@gmail.com>
2020-11-05 00:06:02 +0300

    remove temporary code

--- src/App/screens/Wallet/index.tsx
+++ src/App/screens/Wallet/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Carousel } from '../../../ui-kit/elements';
-import Dropdown from '../../../ui-kit/elements/Dropdown';
 
 const items = ['one', 'two', 'three'];
 
@@ -13,16 +12,6 @@ const WalletScreen: React.FC = React.memo(() => {
 		<>
 			<div style={ { height: '50%' } }>
 				<Carousel items={ items } />
-			</div>
-			<Dropdown<string>
-				options={ [
-					{ value: 'la', display: 'la' },
-					{ value: 'lala', display: 'lala' },
-					{ value: 'blah', display: 'blah' },
-				] }
-			/>
-			<div>
-				kek
 			</div>
 		</>
 	);
